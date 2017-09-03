b5b269fdf2f05b34e2a807c1232425ae177d1bc6
EddieTheHead <hubert.bossy@wp.pl>
2017-09-03 19:06:23 +0200

    add animation

--- corolfulcircles/src/main/java/pl/air/bossy/tutorials/javafx/corolfulcircles/ColorfulCircles.java
+++ corolfulcircles/src/main/java/pl/air/bossy/tutorials/javafx/corolfulcircles/ColorfulCircles.java
@@ -1,8 +1,13 @@
 package pl.air.bossy.tutorials.javafx.corolfulcircles;
 
+import static java.lang.Math.random;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.effect.BoxBlur;
@@ -13,6 +18,7 @@ import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.StrokeType;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 public class ColorfulCircles extends Application {
 
@@ -34,7 +40,7 @@ public class ColorfulCircles extends Application {
 								new Stop(0.85, Color.web("#ef504c")), new Stop(1, Color.web("#f2660f")), }));
 		backGradient.widthProperty().bind(scene.widthProperty());
 		backGradient.heightProperty().bind(scene.heightProperty());
-				
+
 		Group circles = new Group();
 		for (int i = 0; i < 30; ++i) {
 			Circle c = new Circle(150, Color.web("white"));
@@ -45,12 +51,23 @@ public class ColorfulCircles extends Application {
 		}
 		circles.setEffect(new BoxBlur(10, 10, 3));
 
-		Group blendModeGroup = new Group( new Group(
-				new Rectangle(scene.getWidth(),scene.getHeight(),Color.BLACK),circles),
-				backGradient);
+		Group blendModeGroup = new Group(
+				new Group(new Rectangle(scene.getWidth(), scene.getHeight(), Color.BLACK), circles), backGradient);
 		backGradient.setBlendMode(BlendMode.OVERLAY);
 		root.getChildren().add(blendModeGroup);
-		
+
+		// Animation
+		Timeline timeline = new Timeline();
+		for (Node circle : circles.getChildren()) {
+			timeline.getKeyFrames()
+					.addAll(new KeyFrame(Duration.ZERO, // start position at 0
+								new KeyValue(circle.translateXProperty(), random() * 800),
+								new KeyValue(circle.translateYProperty(), random() * 600)),
+							new KeyFrame(new Duration(40000), 
+									new KeyValue(circle.translateXProperty(), random() * 800),
+									new KeyValue(circle.translateYProperty(), random() * 600)));
+		}
+		timeline.play();
 		primaryStage.show();
 	}
 
